ae3d59ad7d83134db472fda917b1d0f8d72929eb
fukagawak <fukagawak@alice.aandt.co.jp>
2025-05-12 13:16:14 +0900

    CORS対応でPUTが抜けてた

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 // CORS用。あくまでも手元で確認するため用なので実運用では使わないこと。
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // 全てのドメインからのアクセスを許可
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
